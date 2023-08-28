42fe3e239fd680777a1142debcb5477e51e7d613
ARUP GOPE <gopea999@gmail.com>
2023-08-28 12:59:08 +0530

    updated

--- src/components/Middlearea/cardsM.jsx
+++ src/components/Middlearea/cardsM.jsx
@@ -4,15 +4,15 @@ import { PiBookmarkSimpleDuotone } from 'react-icons/pi';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ThreeDots } from "react-loader-spinner";
-// const apiKey = import.meta.env.VITE_REACT_APP_NewsApi;
-const apiKey = "404ddb67366240cf8005e18c52c7fc1b"
+const apiKey = import.meta.env.VITE_REACT_APP_NewsApi;
+
 const API = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
 function cardsM() {
   const [news, setNews] = useState([]);
   const [isError, setIsError] = useState("");
   const [loading, setLoading] = useState(true);
-  
+
 
 
   const getNewsData = async () => {
@@ -38,14 +38,14 @@ function cardsM() {
         {loading ? <div className="w-full flex justify-center items-center h-96"><ThreeDots height={40} color="black" /></div> :
           news?.map((val, e) => {
             return (
-              <div key={e}  className='w-[35rem] h-[24rem] relative mt-6 gap-x-2 max-w-[16rem] items-end justify-start rounded-lg '>
+              <div key={e} className='w-[35rem] h-[24rem] relative mt-6 gap-x-2 max-w-[16rem] items-end justify-start rounded-lg '>
                 <div>
-                <img
-                  src={val.urlToImage}
-                  alt="No Image"
-                  className='w-full h-full object-cover absolute top-0 left-0 rounded-2xl '
-                />
-                <div className='absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded-2xl'></div>
+                  <img
+                    src={val.urlToImage}
+                    alt="No Image"
+                    className='w-full h-full object-cover absolute top-0 left-0 rounded-2xl '
+                  />
+                  <div className='absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded-2xl'></div>
                 </div>
                 <div className='absolute flex flex-row justify-between gap-x-32'>
                   <button className=' ml-4 mt-2 bg-gray-300 rounded-3xl w-auto p-1'>{val.source.id}</button>
